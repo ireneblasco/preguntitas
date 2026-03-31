66095eb47c526b18d9a3f4f1cad122f9f3b2e8cd
ireneblasco <ireneblascom@gmail.com>
2026-03-30 20:13:19 -0700

    eas config

--- app.config.js
+++ app.config.js
@@ -7,6 +7,10 @@ module.exports = {
   expo: {
     ...base.expo,
     extra: {
+      ...base.expo.extra,
+      eas: {
+        projectId: 'ce7e9cfd-a831-4d85-ac91-c976f7e9f128',
+      },
       notionApiKey: process.env.NOTION_API_KEY ?? null,
       notionDatabaseId: process.env.NOTION_DATABASE_ID ?? null,
     },
