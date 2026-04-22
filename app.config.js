73ed78c702fdb7221a6e1071b0a0be0b7c65bfe0
ireneblasco <ireneblascom@gmail.com>
2026-04-21 21:01:08 -0700

    Remove notionApiKey from app.config.js to streamline configuration

--- app.config.js
+++ app.config.js
@@ -39,7 +39,6 @@ module.exports = {
       eas: {
         projectId: 'ce7e9cfd-a831-4d85-ac91-c976f7e9f128',
       },
-      notionApiKey: process.env.NOTION_API_KEY ?? null,
       notionDatabaseId: process.env.NOTION_DATABASE_ID ?? null,
     },
   },
